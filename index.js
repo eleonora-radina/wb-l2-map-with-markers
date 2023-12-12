991afc1a870ace38983d67412bf18830416633ed
Eleonora Nikerina <eleonora.radina@mail.ru>
2023-12-12 04:29:26 +0400

    add search

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-//Определяем карту, координаты центра и начальный масштаб
+// определяем карту, координаты центра и начальный масштаб
 const map = L.map('map').setView([55.755864, 37.617698], 13);
 
-//Добавляем на нашу карту слой OpenStreetMap
+// слой OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
+// группы слоев для показа на карте (весь и с фильтром)
 const markersLayer = L.layerGroup();
+const markersLayerFilter = L.layerGroup();
 markersLayer.addTo(map);
 
-function setIconColor(iconColor) {
-  return icon = new L.Icon({
+// установка картинки на маркер в зависимости от цвета
+function setIcon(iconColor) {
+  return new L.Icon({
     iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${iconColor}.png`,
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
@@ -18,33 +21,33 @@ function setIconColor(iconColor) {
   });
 }
 
-function deleteMarker(markerId) {
-  const marker = markersLayer.getLayers().find(el => el._leaflet_id === markerId);
-  markersLayer.removeLayer(marker);
-  console.log(marker);
-  saveMarkers();
-  //map.removeLayer(marker);
+// найти маркер по айди
+function findMarkerById(markerId) {
+  return markersLayer.getLayers().find(el => el._leaflet_id === markerId);
 }
 
+// попап для маркера
 function createPopupContent(name, type, description, markerId) {
   return `
     <strong>${name}</strong><br>
-    Type: ${type}<br>
-    Description: ${description}<br>
+    Тип: ${type}<br>
+    Описание: ${description}<br>
+    <button onClick="editMarker(${markerId})" class="deleteBtn">Редактировать</button>
     <button onClick="deleteMarker(${markerId})" class="deleteBtn">Удалить</button>
   `;
 }
 
+// добавить маркер на карту
 function addMarker() {
   const markerName = document.getElementById('marker-name').value;
   const markerType = document.getElementById('marker-type').value;
   const markerDescription = document.getElementById('marker-description').value;
-  const markerColor = document.getElementById('marker-color').value; // Добавлено получение выбранного цвета
+  const markerColor = document.getElementById('marker-color').value;
 
   if (markerName) {
     const marker = L.marker(map.getCenter(), {
       draggable: true,
-      icon: setIconColor(markerColor),
+      icon: setIcon(markerColor),
       iconColor: markerColor,
       name: markerName,
       type: markerType,
@@ -53,43 +56,78 @@ function addMarker() {
       .addTo(markersLayer);
 
     marker.bindPopup(createPopupContent(markerName, markerType, markerDescription, marker._leaflet_id));
-
-    marker.on('dragend', function (event) {
-      //const { lat, lng } = event.target.getLatLng();
-      saveMarkers();
-    });
+    marker.on('dragend', saveMarkers);
 
     saveMarkers();
     clearForm();
   }
 }
 
+// удалить маркер
+function deleteMarker(markerId) {
+  const marker = findMarkerById(markerId);
+  if (marker) {
+    markersLayer.removeLayer(marker);
+    saveMarkers();
+  }
+}
+
+// редактирование информации о маркере
+function editMarker(markerId) {
+  const marker = findMarkerById(markerId);
+  showEditContainer();
+
+  const editButton = document.getElementById("edit-button");
+
+  const markerEditedName = document.getElementById('edit-marker-name');
+  const markerEditedType = document.getElementById('edit-marker-type');
+  const markerEditedDescription = document.getElementById('edit-marker-description');
+  const markerEditedColor = document.getElementById('edit-marker-color');
+
+  markerEditedName.value = marker.options.name;
+  markerEditedType.value = marker.options.type;
+  markerEditedDescription.value = marker.options.description;
+  markerEditedColor.value = marker.options.iconColor;
+
+  function editFunction() {
+    marker.setIcon(setIcon(markerEditedColor.value));
+    marker.bindPopup(createPopupContent(markerEditedName.value, markerEditedType.value, markerEditedDescription.value, marker._leaflet_id));
+
+    marker.options.name = markerEditedName.value;
+    marker.options.iconColor = markerEditedColor.value;
+    marker.options.type = markerEditedType.value;
+    marker.options.description = markerEditedDescription.value;
+
+    saveMarkers();
+    clearEditForm();
+    hideEditContainer();
+    editButton.removeEventListener("click", editFunction);
+  }
+
+  editButton.addEventListener("click", editFunction);
+}
+
+// сохранение маркеров в локалсторадж
 function saveMarkers() {
   const markersData = markersLayer.getLayers().map(marker => {
     const { lat, lng } = marker.getLatLng();
     return {
-      //id: marker._leaflet_id,
       name: marker.options.name,
       type: marker.options.type,
       description: marker.options.description,
       iconColor: marker.options.iconColor,
       lat,
-      lng,
+      lng
     };
   });
   localStorage.setItem('markers', JSON.stringify(markersData));
 }
 
-function clearForm() {
-  document.getElementById('marker-name').value = '';
-  document.getElementById('marker-type').value = '';
-  document.getElementById('marker-description').value = '';
-}
-
+// добавление маркеров из локалсторадж
 const savedMarkers = JSON.parse(localStorage.getItem('markers')) || [];
 savedMarkers.forEach(markerData => {
   const marker = L.marker([markerData.lat, markerData.lng], {
-    icon: setIconColor(markerData.iconColor),
+    icon: setIcon(markerData.iconColor),
     draggable: true,
     type: markerData.type,
     description: markerData.description,
@@ -97,10 +135,79 @@ savedMarkers.forEach(markerData => {
     name: markerData.name,
   })
     .addTo(markersLayer);
-    
+
   marker.bindPopup(createPopupContent(markerData.name, markerData.type, markerData.description, marker._leaflet_id));
-  marker.on('dragend', function (event) {
-    //const { lat, lng } = event.target.getLatLng();
-    saveMarkers();
-  });
+  marker.on('dragend', saveMarkers);
 });
+
+// фильтрация маркеров по тексту в описании
+function filterMarkersByDescription(searchText) {
+  markersLayerFilter.removeFrom(map);
+  markersLayerFilter.clearLayers();
+  markersLayer.removeFrom(map);
+
+  const filteredMarkers = markersLayer.getLayers().filter(marker => {
+    const description = marker.options.description.toLowerCase();
+    return description.includes(searchText.toLowerCase());
+  });
+
+  filteredMarkers.forEach(filteredMarker => {
+    filteredMarker.addTo(markersLayerFilter);
+  });
+
+  markersLayerFilter.addTo(map);
+
+  console.log(markersLayer, markersLayerFilter);
+}
+
+// обработчик события для кнопки поиска
+function searchMarkers() {
+  const searchText = document.getElementById('search-text').value;
+  filterMarkersByDescription(searchText);
+}
+
+// сброс поиска
+function resetSearch() {
+  markersLayer.addTo(map);
+  document.getElementById('search-text').value = "";
+  console.log(markersLayer, markersLayerFilter);
+}
+
+// показать блок с редактированием 
+function showEditContainer() {
+  const editContainer = document.querySelector(".container_edit");
+  const addContainer = document.querySelector(".container_add");
+  const map = document.getElementById("map");
+
+  editContainer.style.display = "block";
+  addContainer.style.pointerEvents = 'none';
+  map.style.pointerEvents = 'none';
+  addContainer.style.opacity = '0.6';
+  map.style.opacity = '0.6';
+}
+
+// скрыть блок с редактированием 
+function hideEditContainer() {
+  const editContainer = document.querySelector(".container_edit");
+  const addContainer = document.querySelector(".container_add");
+  const map = document.getElementById("map");
+
+  editContainer.style.display = "none";
+  addContainer.style.pointerEvents = 'auto';
+  map.style.pointerEvents = 'auto';
+  addContainer.style.opacity = '1';
+  map.style.opacity = '1';
+}
+
+// очистки форм
+function clearForm() {
+  document.getElementById('marker-name').value = '';
+  document.getElementById('marker-type').value = '';
+  document.getElementById('marker-description').value = '';
+}
+
+function clearEditForm() {
+  document.getElementById('edit-marker-name').value = '';
+  document.getElementById('edit-marker-type').value = '';
+  document.getElementById('edit-marker-description').value = '';
+}
